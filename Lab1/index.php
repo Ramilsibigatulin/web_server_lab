42fb37ccf7501e07781273b3f471ba2fc8955318
Ramilsibigatulin <sibigatulinramil@gmail.com>
2024-02-06 14:33:53 +0300

    first-commit

---
+++ Lab1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+     echo'<h1>Hello, PHP</h1>'; 
+    $a = 10;
+    $b = 20;
+    define('pi',3.14);
+    $x = $a * pi;
+    echo "<h2>$a</h2>"
+    ?>
+</body>
+</html>
